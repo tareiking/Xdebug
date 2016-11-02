8d41250636342f03bb47041620bc704e691694bd
Bronson Quick <bronson@sennza.com.au>
2016-11-02 20:01:40 +1000

    Add support for Php 5.6 and 7.0

--- modules/xdebug/manifests/init.pp
+++ modules/xdebug/manifests/init.pp
@@ -1,20 +1,43 @@
 class xdebug (
   $path        = "/vagrant/extensions/xdebug",
-  $remote_host = $chassis_config[hosts]
+  $remote_host = $chassis_config[hosts],
+  $php_version = $chassis_config[php]
 ) {
   $hosts = join($chassis_config[hosts],",")
-  package { 'php5-xdebug':
-    ensure => latest,
-    require => Package['php5-fpm']
-  }
+  if versioncmp( "${php_version}", '5.6') < 1 {
+    package { 'php5-xdebug':
+        ensure => latest,
+        require => Package['php5-fpm']
+    }
 
-  file { '/etc/php5/fpm/conf.d/xdebug.ini':
-    content => template('xdebug/xdebug.ini.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0644,
-    require => Package['php5-fpm','php5-xdebug'],
-    ensure  => 'present',
-    notify  => Service['php5-fpm'],
-  }
+    file { '/etc/php5/fpm/conf.d/xdebug.ini':
+      content => template('xdebug/xdebug.ini.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => 0644,
+      require => Package['php5-fpm','php5-xdebug'],
+      ensure  => 'present',
+      notify  => Service['php5-fpm'],
+    }
+  } else {
+    package { 'php-xdebug':
+      ensure => latest,
+      require => Package["php${php_version}-fpm"],
+      notify  => Service["php${php_version}-fpm"],
+    }
+
+    file { "/etc/php/${php_version}/fpm/conf.d/xdebug.ini":
+      content => template('xdebug/xdebug.ini.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => 0644,
+      require => Package["php${php_version}-fpm",'php-xdebug'],
+      ensure  => 'present',
+      notify  => Service["php${php_version}-fpm"],
+    }
+
+    package { 'php5-fpm':
+        ensure => absent
+      }
+    }
 }
